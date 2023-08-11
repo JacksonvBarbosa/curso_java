b5fc53b099d430af5e1993e8f58d82ca78d37a26
Jackson Ventura <jacksonvbarbosa@terra.com.br>
2023-08-11 02:42:13 -0300

    Pasta treino string e inteiro adicionado a branch

---
+++ string_inteiro_treino/stringInteiro.java
@@ -0,0 +1,11 @@
+public class stringInteiro {
+    public static void main(String[] args) {
+
+        String saudacao = "Olá, meu nome é ";
+        String nome = "Jackson ";
+        String continuacao = "e minha idade é ";
+        byte idade = 39;
+        System.out.println(saudacao + nome + continuacao + idade);
+
+    }
+}
